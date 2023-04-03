f9c29c3d8caa5b706a91948ac410bf4f4af0b057
Bruno Cezario <bruno.cezario@bancobmg.com.br>
2023-04-03 13:24:02 -0300

    Criação da tela de login

---
+++ SmartIO/Data/SmartIOContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SmartIO.Models;
+
+namespace SmartIO.Data
+{
+    public class SmartIOContext : DbContext
+    {
+        public SmartIOContext (DbContextOptions<SmartIOContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SmartIO.Models.Usuario> Usuario { get; set; } = default!;
+    }
+}
---
+++ SmartIO/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Build.Framework;
+
+namespace SmartIO.Models
+{
+    public class Usuario
+    {
+        
+        public int Id { get; set; }
+
+        [Required]
+        public string? Nome { get; set; }
+        public string? login { get; set; }
+        public string? password { get; set; }
+        [Required]
+        public string? Cep { get; set; }
+        public int? IdFormulario { get; set; }
+    }
+}
