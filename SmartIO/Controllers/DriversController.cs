3924102b27c51c6e216c06bbb674a39e646b797e
Bruno Cezario <bruno.cezario@bancobmg.com.br>
2023-04-04 13:53:19 -0300

    Ajuste na criação da tela dso drivers

---
+++ SmartIO/Controllers/DriversController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartIO.Controllers
+{
+    public class DriversController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
